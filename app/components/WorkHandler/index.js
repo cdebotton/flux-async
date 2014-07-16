b7ad5740862223ca87cbde897adeef7e4da8b954
Christian de Botton <cdebotton@gmail.com>
2014-07-15 23:29:15 -0400

    Fixing sample

--- app/components/WorkHandler/index.js
+++ app/components/WorkHandler/index.js
@@ -8,9 +8,11 @@ module.exports = React.createClass({
     return (
       <div className="work">
         <h2>Work</h2>
-        <Link to="interactive">Interactive</Link>
-        <Link to="video">Video</Link>
-        <Link to="photography">Photography</Link>
+        <nav>
+          <Link to="interactive">Interactive</Link>
+          <Link to="video">Video</Link>
+          <Link to="photography">Photography</Link>
+        </nav>
         {this.props.activeRoute}
       </div>
     );
