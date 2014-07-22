84b07133b5b25e7c0da830f65cb18f3fc60af502
Christian de Botton <cdebotton@gmail.com>
2014-07-21 22:26:50 -0400

    Watchify.

--- app/components/Routes/index.js
+++ app/components/Routes/index.js
@@ -9,17 +9,7 @@ module.exports = function() {
   return (
     <Route handler={App}>
       <Route name="/" handler={IndexHandler} />
-<<<<<<< HEAD
       <Route name="admin" handler={AdminHandler} />
-=======
-      <Route name="work" handler={WorkHandler}>
-        <Route name="interactive" handler={InteractiveHandler} />
-        <Route name="video" handler={VideoHandler} />
-        <Route name="photography" handler={PhotographyHandler} />
-      </Route>
-      <Route name="admin" handler={AdminHandler} />
-      <Route name="about" handler={AboutHandler} />
->>>>>>> 85ec0693594c416dda08916f62a09914ae3a5439
     </Route>
   );
 };
